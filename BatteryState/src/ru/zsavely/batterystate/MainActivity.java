ac26ebefef8d4122632aef24e23fa3b5457ecc78
Savely Zagursky <savva-5@mail.ru>
2014-10-10 13:54:31 +0400

    Added toast on start

--- BatteryState/src/ru/zsavely/batterystate/MainActivity.java
+++ BatteryState/src/ru/zsavely/batterystate/MainActivity.java
@@ -2,12 +2,16 @@ package ru.zsavely.batterystate;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Toast.makeText(this,
+				"USB Plugged:     10/30 minutes;\nUSB Unplugged: 1 minute.",
+				Toast.LENGTH_SHORT).show();
 		finish();
 	}
 
